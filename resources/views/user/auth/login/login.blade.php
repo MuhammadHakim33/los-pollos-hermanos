9efba7ebd9603865f323151fd17d25a3f44b67ca
Fathanrachel <racheltambunan10@gmail.com>
2024-12-02 12:45:15 +0700

    update input login

--- resources/views/user/auth/login/login.blade.php
+++ resources/views/user/auth/login/login.blade.php
@@ -36,34 +36,44 @@
 
       <!-- Email -->
       <div class="relative mb-5">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          class="peer p-4 block w-full bg-gray-100 border border-gray-300 rounded-lg text-sm placeholder-transparent focus:border-black focus:ring-2 focus:ring-black focus:outline-none"
-          placeholder="you@email.com" />
-        <label
-          for="email"
-          class="absolute left-4 top-4 text-gray-400 text-sm transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-black">
-          Email
-        </label>
+        <input type="email" id="email" name="email" class="peer p-4 block w-full bg-gray-100 border border-gray-300 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none  dark:border-neutral-700 dark:text-neutral-800 dark:focus:ring-neutral-600
+        focus:pt-6
+        focus:pb-2
+        [&:not(:placeholder-shown)]:pt-6
+        [&:not(:placeholder-shown)]:pb-2
+        autofill:pt-6
+        autofill:pb-2" placeholder="you@email.com">
+        <label for="email" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] dark:text-black peer-disabled:opacity-50 peer-disabled:pointer-events-none
+        peer-focus:scale-90
+        peer-focus:translate-x-0.5
+        peer-focus:-translate-y-1.5
+        peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+        peer-[:not(:placeholder-shown)]:scale-90
+        peer-[:not(:placeholder-shown)]:translate-x-0.5
+        peer-[:not(:placeholder-shown)]:-translate-y-1.5
+        peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-800 dark:text-neutral-500">Email</label>
         @error('email')
         <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
         @enderror
       </div>
 
       <div class="relative mb-5">
-        <input
-          type="password"
-          name="password"
-          id="password"
-          class="peer p-4 block w-full bg-gray-100 border border-gray-300 rounded-lg text-sm placeholder-transparent focus:border-black focus:ring-2 focus:ring-black focus:outline-none"
-          placeholder="********" />
-        <label
-          for="password"
-          class="absolute left-4 top-4 text-gray-400 text-sm transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-black">
-          Password
-        </label>
+        <input type="password" id="password" name="password" class="peer p-4 block w-full bg-gray-100 border border-gray-300 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-800 dark:focus:ring-neutral-600
+        focus:pt-6
+        focus:pb-2
+        [&:not(:placeholder-shown)]:pt-6
+        [&:not(:placeholder-shown)]:pb-2
+        autofill:pt-6
+        autofill:pb-2" placeholder="********">
+        <label for="password" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] dark:text-black peer-disabled:opacity-50 peer-disabled:pointer-events-none
+        peer-focus:scale-90
+        peer-focus:translate-x-0.5
+        peer-focus:-translate-y-1.5
+        peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+        peer-[:not(:placeholder-shown)]:scale-90
+        peer-[:not(:placeholder-shown)]:translate-x-0.5
+        peer-[:not(:placeholder-shown)]:-translate-y-1.5
+        peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 dark:text-neutral-500">Password</label>
         @error('password')
         <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
         @enderror
