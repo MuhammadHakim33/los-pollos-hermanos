71478c1d16eb163d1136dc7a5e42cea240c55acd
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2024-12-04 15:46:12 +0700

    update cart and order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -47,38 +47,38 @@ class OrderController extends Controller
         $method_payment = $request->method_payment;
         $items = [];
 
-        DB::transaction(function () use($id_user, $carts) {
-            $order = Order::create([
-                'id' => $this->generateOrderCode(),
-                'id_user' => $id_user,
-                'status' => 'pending'
-            ]);
+        // DB::transaction(function () use($id_user, $carts) {
+        //     $order = Order::create([
+        //         'id' => $this->generateOrderCode(),
+        //         'id_user' => $id_user,
+        //         'status' => 'pending'
+        //     ]);
 
-            foreach($carts as $cart) {
-                $item = [];
-                $item['id_order'] = $order->id;
-                $item['id_menu'] = $cart->id;
-                $item['qty'] = $cart->qty;
-                $item['price'] = $cart->price;
-                $items[] = $item;
-            }
+        //     foreach($carts as $cart) {
+        //         $item = [];
+        //         $item['id_order'] = $order->id;
+        //         $item['id_menu'] = $cart->id;
+        //         $item['qty'] = $cart->qty;
+        //         $item['price'] = $cart->price;
+        //         $items[] = $item;
+        //     }
 
-            ItemOrder::create($items);
+        //     ItemOrder::create($items);
 
-            Delivery::create([
-                'id' => Str::ulid(),
-                'id_order' => $order->id,
-                'status' => 'pending'
-            ]);
+        //     Delivery::create([
+        //         'id' => Str::ulid(),
+        //         'id_order' => $order->id,
+        //         'status' => 'pending'
+        //     ]);
 
-            Payment::create([
-                'id' => $this->generatePaymentCode(),
-                'id_order' => $order->id,
-                'method_payment' => $method_payment,
-                'total' => $total,
-                'status' => 'pending'
-            ]);
-        });
+        //     Payment::create([
+        //         'id' => $this->generatePaymentCode(),
+        //         'id_order' => $order->id,
+        //         'method_payment' => $method_payment,
+        //         'total' => $total,
+        //         'status' => 'pending'
+        //     ]);
+        // });
     }
 
     function generateOrderCode() 
