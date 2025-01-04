17f9c70e9fe26f1f1adbc24fd0e90e37bfaffb58
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2025-01-04 18:13:04 +0700

    update detail order view

--- resources/views/components/payment-alert/index.blade.php
+++ resources/views/components/payment-alert/index.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'mb-6 border rounded p-4']) }} role="alert" tabindex="-1" aria-labelledby="hs-actions-label">
+<div {{ $attributes->merge(['class' => 'border rounded p-4']) }} role="alert" tabindex="-1" aria-labelledby="hs-actions-label">
     <div class="flex flex-col sm:flex-row items-start sm:items-center justify-start gap-3">
         {{ $slot }}
     </div>
