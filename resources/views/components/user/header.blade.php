7c42a3bedd5816276e2b763591ebfbc055920fa5
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2025-06-13 08:23:36 +0700

    update header (not complete)

--- resources/views/components/user/header.blade.php
+++ resources/views/components/user/header.blade.php
@@ -4,9 +4,32 @@ $isCartEmpty = count($carts) > 0 ? true : false ;
 
 
 <header class="bg-white sticky top-0 inset-x-0 border-b shadow-sm z-10">
-    <div class="container mx-auto px-4 py-4 flex justify-between items-center">
+    <div class="container mx-auto px-4 py-4 flex items-center">
         <div class="flex items-center">
-            <img alt="Selera Panca Logo" class="h-14 rounded-xl" src="{{ asset('images/Logo RM. Selera Panca.png') }}" />
+            <img alt="Selera Panca Logo" class="h-14 rounded-xl" src="{{ asset('images/los_pollos_hermanos_2.png') }}" />
+            <p>Cabang</p>
+            <div class="hs-dropdown relative inline-flex">
+                <button id="hs-dropdown-default" type="button" class="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                    Actions
+                    <svg class="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
+                </button>
+                <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full" role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-default">
+                    <div class="p-1 space-y-0.5">
+                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100" href="#">
+                            Newsletter
+                        </a>
+                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100" href="#">
+                            Purchases
+                        </a>
+                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100" href="#">
+                            Downloads
+                        </a>
+                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100" href="#">
+                            Team Account
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="flex items-center gap-x-4">
             @auth
@@ -44,7 +67,7 @@ $isCartEmpty = count($carts) > 0 ? true : false ;
                             <i class="ri-user-3-line"></i> My Profile <i class="ri-arrow-right-wide-fill ml-auto"></i>
                         </a>
                         <a class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="/order/history">
-                            <i class="ri-shopping-basket-line"></i> Purchases <i class="ri-arrow-right-wide-fill ml-auto"></i>                                
+                            <i class="ri-shopping-basket-line"></i> Purchases <i class="ri-arrow-right-wide-fill ml-auto"></i>
                         </a>
                         <form action="{{ route('logout') }}" method="post" class="flex">
                             @csrf
